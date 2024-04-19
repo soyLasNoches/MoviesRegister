c7f3fd2d0d070055eab22c6ce43d177df9b1bd57
Isabela-Melo <163340635+Isabela-Melo@users.noreply.github.com>
2024-04-18 23:39:24 -0300

    Add files via upload

---
+++ PrincipalForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MoviesRegister
+{
+    partial class PrincipalForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PrincipalForm));
+            this.panelMovies = new System.Windows.Forms.Panel();
+            this.flpMovies = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.bdsImovel = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelMovies.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsImovel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMovies
+            // 
+            this.panelMovies.AutoScroll = true;
+            this.panelMovies.AutoSize = true;
+            this.panelMovies.Controls.Add(this.flpMovies);
+            this.panelMovies.Controls.Add(this.panel4);
+            this.panelMovies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMovies.Location = new System.Drawing.Point(0, 10);
+            this.panelMovies.Name = "panelMovies";
+            this.panelMovies.Size = new System.Drawing.Size(848, 697);
+            this.panelMovies.TabIndex = 1;
+            // 
+            // flpMovies
+            // 
+            this.flpMovies.AutoScroll = true;
+            this.flpMovies.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flpMovies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpMovies.Location = new System.Drawing.Point(0, 0);
+            this.flpMovies.Name = "flpMovies";
+            this.flpMovies.Size = new System.Drawing.Size(848, 670);
+            this.flpMovies.TabIndex = 64;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.lblQuantidade);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 670);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(848, 27);
+            this.panel4.TabIndex = 63;
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(7, 7);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(74, 13);
+            this.lblQuantidade.TabIndex = 0;
+            this.lblQuantidade.Text = "Quantidade: 0";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(848, 10);
+            this.panel1.TabIndex = 0;
+            // 
+            // PrincipalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 707);
+            this.Controls.Add(this.panelMovies);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PrincipalForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro de Filmes";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PrincipalForm_FormClosing);
+            this.Load += new System.EventHandler(this.PrincipalForm_Load);
+            this.panelMovies.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsImovel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panelMovies;
+        private System.Windows.Forms.BindingSource bdsImovel;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.FlowLayoutPanel flpMovies;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ PrincipalForm.cs
@@ -0,0 +1,201 @@
+﻿using Newtonsoft.Json;
+using MoviesRegister.Base;
+using MoviesRegister.BLL;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using MoviesRegister.UI;
+
+namespace MoviesRegister
+{
+    public partial class PrincipalForm : BaseForm
+    {
+        public PrincipalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PrincipalForm_Load(object sender, EventArgs e)
+        {
+            lblQuantidade.Text = "";
+            LocalizarFilmes();
+        }
+
+        private void PrincipalForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult resposta = MessageBox.Show("Deseja realmente fechar o sistema?",
+                    "MoviesRegister",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+            if (resposta == DialogResult.Yes)
+            {
+
+            }
+            else
+            {
+                e.Cancel = true;
+                OnClosing(e);
+            }
+        }
+
+        private bool VerificarExistenciaFilme(int id)
+        {
+            if (Movie.GetMovieById(id) == null)
+                return true;
+            else
+                return false;
+        }
+
+        private Panel NovoPainel(int id, string title, string urlImagem, string releaseDate, Movie movie)
+        {
+            Panel panel = new Panel();
+            panel.Height = 400;
+            panel.Width = 250;
+            panel.Margin = new Padding(10);
+            panel.BorderStyle = BorderStyle.None;
+
+            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.BackColor = Color.WhiteSmoke;
+            tableLayoutPanel.Padding = new Padding(10);
+            tableLayoutPanel.ColumnCount = 1;
+            tableLayoutPanel.RowCount = 2;
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            tableLayoutPanel.RowStyles.RemoveAt(1);
+
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.Dock = DockStyle.Fill;
+
+            try
+            {
+                pictureBox.LoadCompleted += (s, e) =>
+                {
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                };
+
+                pictureBox.LoadAsync(urlImagem);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao carregar a imagem: " + ex.Message);
+            }
+
+            Label titleLabel = new Label();
+            titleLabel.Text = title;
+            titleLabel.AutoSize = true;
+            titleLabel.Font = new Font(titleLabel.Font.FontFamily, 12, FontStyle.Bold);
+            titleLabel.ForeColor = Color.Black;
+
+            Label releaseDateLabel = new Label();
+            releaseDateLabel.Text = releaseDate;
+            releaseDateLabel.AutoSize = true;
+            releaseDateLabel.Font = new Font(releaseDateLabel.Font.FontFamily, 9);
+            releaseDateLabel.ForeColor = Color.Gray;
+
+            Label favorito = new Label();
+            favorito.BackColor = Color.WhiteSmoke;
+            favorito.ForeColor = Color.Black;
+            favorito.FlatStyle = FlatStyle.Flat;
+            favorito.Image = VerificarExistenciaFilme(id) ? null : Properties.Resources.favoritar;
+            favorito.Size = new Size(20, 20);
+
+            Button button = new Button();
+            button.Text = "Detalhes";
+            button.BackColor = Color.LightGray;
+            button.ForeColor = Color.Black;
+            button.FlatStyle = FlatStyle.Flat;
+            button.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button.FlatAppearance.BorderSize = 0;
+
+            Panel footer = new Panel();
+            button.Location = new Point((footer.Width - button.Width), 0);
+            favorito.Location = new Point(0, 0);
+            footer.Controls.Add(favorito);
+            footer.Controls.Add(button);
+            footer.Dock = DockStyle.Fill;
+            footer.Height = 25;
+
+            button.Click += (sender, e) =>
+            {
+                int pos = flpMovies.VerticalScroll.Value;
+
+                MovieDetails(movie);
+                LocalizarFilmes();
+
+                flpMovies.VerticalScroll.Value = pos;
+            };
+
+            tableLayoutPanel.Controls.Add(pictureBox, 0, 0);
+            tableLayoutPanel.Controls.Add(titleLabel, 0, 1);
+            tableLayoutPanel.Controls.Add(releaseDateLabel, 0, 2);
+            tableLayoutPanel.Controls.Add(footer, 0, 3);
+
+            panel.Controls.Add(tableLayoutPanel);
+
+            panel.Paint += (sender, e) =>
+            {
+                using (GraphicsPath path = new GraphicsPath())
+                {
+                    Rectangle bounds = panel.ClientRectangle;
+                    bounds.Width--;
+                    bounds.Height--;
+                    int radius = 10;
+                    path.AddArc(bounds.X, bounds.Y, radius, radius, 180, 90);
+                    path.AddArc(bounds.X + bounds.Width - radius, bounds.Y, radius, radius, 270, 90);
+                    path.AddArc(bounds.X + bounds.Width - radius, bounds.Y + bounds.Height - radius, radius, radius, 0, 90);
+                    path.AddArc(bounds.X, bounds.Y + bounds.Height - radius, radius, radius, 90, 90);
+                    panel.Region = new Region(path);
+                }
+            };
+
+            return panel;
+        }
+
+        private void MovieDetails(Movie movie)
+        {
+            MoviesForm moviesForm = new MoviesForm(movie);
+            moviesForm.ShowDialog();
+        }
+
+        private void LocalizarFilmes()
+        {
+            this.flpMovies.Controls.Clear();
+
+            SortableBindingList<MoviesHelper> movies = new SortableBindingList<MoviesHelper>();
+
+            movies = MoviesHelper.GetMovies();
+
+            string urlImagem = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
+
+            foreach (MoviesHelper item in movies)
+            {
+                Movie movie = new Movie();
+
+                movie.Id = item.id;
+                movie.Original_language = item.original_language;
+                movie.Original_title = item.original_title;
+                movie.Popularity = item.popularity;
+                movie.Poster_path = item.poster_path;
+                movie.Release_date = item.release_date;
+                movie.Title = item.title;
+                movie.Vote_average = item.vote_average;
+
+                this.flpMovies.Controls.Add(NovoPainel(item.id, item.title.Length > 25 ? item.title.Substring(0, 25) + "..." : item.title, urlImagem + item.poster_path, item.release_date.ToString().Substring(0, 10), movie));
+            }
+
+            lblQuantidade.Text = "Quantidade: " + movies.Count.ToString();
+        }
+    }
+}
